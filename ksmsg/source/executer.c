5f311156843b45ff224e2b8b57ad7f0e4f1fa01a
Zolboo <zolboo.deb@gmail.com>
2018-12-26 18:55:32 +0100

    Ksmsg Executer setterfuncion

--- ksmsg/source/executer.c
+++ ksmsg/source/executer.c
@@ -59,7 +59,9 @@ OV_DLLFNCEXPORT void ksmsg_executer_typemethod(OV_INSTPTR_ksbase_ComTask this) {
   Ov_ForEachChildEx(ov_containment, pinbox, pMsg, PostSys_Message) { break; }
   if(!pMsg)
     return;
-  ov_logfile_info("postExec: %s", pMsg->v_msgBody);
+  ov_logfile_info(
+      "postExec: %s:%s:%s: %s", pMsg->v_msgBody, pMsg->v_pathComponent.value[0],
+      pMsg->v_pathComponent.value[1], pMsg->v_pathComponent.value[2]);
   OV_STRING     srv = NULL;
   OV_STRING     op = NULL;
   OV_STRING     contName = NULL;
@@ -79,7 +81,7 @@ OV_DLLFNCEXPORT void ksmsg_executer_typemethod(OV_INSTPTR_ksbase_ComTask this) {
 
   result = Ov_CreateIDedObject(PostSys_Message, pMsgRes, pinst, "Message");
   PostSys_Message_pathLen_set(pMsgRes, pMsg->v_pathLen);
-  pathLen = pMsg->v_pathAddress.veclen;
+  pathLen = pMsg->v_pathLen;
   OV_STRING_VEC* a[3] = {&pMsg->v_pathAddress, &pMsg->v_pathName,
                          &pMsg->v_pathComponent};
   OV_STRING_VEC* b[3] = {&pMsgRes->v_pathAddress, &pMsgRes->v_pathName,
@@ -89,9 +91,10 @@ OV_DLLFNCEXPORT void ksmsg_executer_typemethod(OV_INSTPTR_ksbase_ComTask this) {
       ov_string_setvalue(&b[i]->value[j], a[i]->value[pathLen - 1 - j]);
     }
   }
+  ov_string_setvalue(&pMsgRes->v_refMsgID, pMsg->v_msgID);
   /* beginning */
   if(ov_string_compare(op, "GET") == OV_STRCMP_EQUAL) {
-    ov_logfile_info("ksmsg_executer: called GET");
+    /*ov_logfile_info("ksmsg_executer: called GET");*/
     OV_GETVAR_PAR params = {0};
     ov_memstack_lock();
     params.identifiers_len = ids.veclen;
@@ -106,9 +109,13 @@ OV_DLLFNCEXPORT void ksmsg_executer_typemethod(OV_INSTPTR_ksbase_ComTask this) {
     /*	delete Ticket	*/
     pticket->vtbl->deleteticket(pticket);
 
-    OV_UINT       len = params.identifiers_len;
-    OV_STRING*    emptyStr = ov_memstack_alloc(sizeof(OV_STRING) * len);
-    OV_STRING*    valStr = ov_memstack_alloc(sizeof(OV_STRING) * len);
+    OV_UINT    len = params.identifiers_len;
+    OV_STRING* emptyStr = ov_memstack_alloc(sizeof(OV_STRING) * len);
+    OV_STRING* valStr = ov_memstack_alloc(sizeof(OV_STRING) * len);
+    for(OV_UINT i = 0; i < len; ++i) {
+      emptyStr[i] = NULL;
+      valStr[i] = NULL;
+    }
     OV_STRING_VEC resIds = {len, emptyStr};
     OV_STRING_VEC resUnits = {len, emptyStr};
     OV_STRING_VEC resTypes = {len, emptyStr};
@@ -147,7 +154,7 @@ OV_DLLFNCEXPORT void ksmsg_executer_typemethod(OV_INSTPTR_ksbase_ComTask this) {
     ov_memstack_unlock();
     Ov_DeleteObject(pMsg);
   } else if(ov_string_compare(op, "SET") == OV_STRCMP_EQUAL) {
-    ov_logfile_info("ksmsg_executer: called SET");
+    /*ov_logfile_info("ksmsg_executer: called SET");*/
     OV_SETVAR_RES results = {0};
     OV_SETVAR_PAR params = {0};
     ov_memstack_lock();
@@ -155,11 +162,13 @@ OV_DLLFNCEXPORT void ksmsg_executer_typemethod(OV_INSTPTR_ksbase_ComTask this) {
     OV_UINT len = params.items_len;
     params.items_val = ov_memstack_alloc(len * sizeof(OV_SETVAR_ITEM));
     for(OV_UINT i = 0; i < len; ++i) {
+      params.items_val[i].path_and_name = NULL;
       ov_string_setvalue(&params.items_val[i].path_and_name, ids.value[i]);
       cJSON* jsval = cJSON_Parse(values.value[i]);
       if(!jsval || !cJSON_IsArray(jsval)) {
         ov_logfile_error("ksmsg_executer: %s malformed json val",
                          values.value[i]);
+        continue;
       }
       jsonToVarvalue(&params.items_val[i].var_current_props.value, jsval);
     }
@@ -174,11 +183,12 @@ OV_DLLFNCEXPORT void ksmsg_executer_typemethod(OV_INSTPTR_ksbase_ComTask this) {
     pticket->vtbl->deleteticket(pticket);
 
     OV_STRING* emptyStr = ov_memstack_alloc(sizeof(OV_STRING) * len);
+    OV_STRING* valStr = ov_memstack_alloc(sizeof(OV_STRING) * len);
     for(OV_UINT i = 0; i < len; ++i) {
       emptyStr[i] = NULL;
+      valStr[i] = NULL;
     }
 
-    OV_STRING*    valStr = ov_memstack_alloc(sizeof(OV_STRING) * len);
     OV_STRING_VEC resIds = {len, emptyStr};
     OV_STRING_VEC resUnits = {len, emptyStr};
     OV_STRING_VEC resTypes = {len, emptyStr};
@@ -186,7 +196,28 @@ OV_DLLFNCEXPORT void ksmsg_executer_typemethod(OV_INSTPTR_ksbase_ComTask this) {
     for(OV_UINT i = 0; i < len; ++i) {
       ov_string_print(&resVals.value[i], "%d", results.results_val[i]);
     }
+    msgBody = acplt_simpleMsg_GenerateFlatBody(
+        NULL, "RES:SET", 0, NULL, &resIds, &resVals, &resUnits, &resTypes);
+    result = PostSys_Message_msgBody_set(pMsgRes, msgBody);
+    if(Ov_Fail(result)) {
+      Ov_DeleteObject(pMsgRes);
+      ov_memstack_unlock();
+      return;
+    }
+    pMsgDelivery = Ov_StaticPtrCast(
+        PostSys_MsgDelivery,
+        Ov_GetFirstChild(ov_instantiation, pclass_PostSys_MsgDelivery));
+    if(pMsgDelivery) {
+      Ov_Link(PostSys_MsgDelivery2Message, pMsgDelivery, pMsgRes);
+    }
+
     ov_memstack_unlock();
+    Ov_DeleteObject(pMsg);
     return;
+  } else {
+    ov_logfile_error("ksmsg_executer: unknown operator %s", op);
+    Ov_DeleteObject(pMsg);
   }
+  return;
+}
 
